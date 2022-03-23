5b74559fef902e1d28f0a6722f8a34c5420efbfa
deel7000 <guopengjun1998@163.com>
2022-03-23 23:44:45 +0800

    log

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 
 
 using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
 const int Nt = 9000, Nc = 40, Ns = 200;
 int qos, cntTime, cntClient, cntServer; //最大延迟、总时间、客户数量、服务器数量
 vector<string> client(Nc), server(Ns), timeLog(Nt); //客户名称、服务器名称、时间戳
@@ -16,23 +18,27 @@ int tclient[Nt][Nc], qos_cs[Nc][Ns]; //时间-客户：需要流量大小，  客户-服务器 延
 vector<vector<int>> workband(Nc); //每个客户能连接的服务器
 vector<int> tserver(Ns); //服务器带宽
 unordered_map<string, int> client_idx, server_idx; //名字对应序号
+vector<bool> vis; //服务器是否用过
+vector<ll> sumBand; //每个时刻总流量
+vector<int> sortedTime; //排序时刻
+vector<int> maxServer; //每个服务器最多分配
 
 // 读文件
 void readConfig() {
     //config.ini读入
     ifstream configFile("/data/config.ini");
     string configs;
-   // while (getline(configFile, configs)) cout << configs << endl;
-   
+    // while (getline(configFile, configs)) cout << configs << endl;
+
     getline(configFile, configs);
     getline(configFile, configs);
     string configlog = "qos_constraint=";
     int nconfig = configlog.size();
- //   cout << nconfig << endl;
- //   cout << configs << endl;
+    //   cout << nconfig << endl;
+    //   cout << configs << endl;
     qos = stoi(configs.substr(nconfig));
     configFile.close();
-   
+
 }
 
 void readDemand() {
@@ -149,36 +155,30 @@ void readFile() {
     readConfig();
 }
 
-void debug() {
-    cout << "时间序列长度： " << cntTime << endl;
-    cout << "客户数量： " << cntClient << endl;
-    cout << "客户列表： " << endl;
-    for (int i = 0; i < cntClient; ++i) cout << client[i] << endl;
-    cout << "时序带宽需求（时间-客户）：（按excel顺序对应） " << endl;
-    for (int i = 0; i < cntTime; ++i) {
-        for (int j = 0; j < cntClient; ++j) {
-            cout << tclient[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << "服务器数量： " << cntServer << endl;
-    cout << "服务器名称及带宽" << endl;
-    for (int i = 0; i < cntServer; ++i) cout << server[i] << " " << tserver[i] << endl;
-    cout << "qos: " << qos << endl;
-    cout << "服务器-客户对应延迟： （按excel顺序对应）" << endl;
-    for (int i = 0; i < cntServer; ++i) {
-        for (int j = 0; j < cntClient; ++j) {
-            cout << qos_cs[j][i] << " ";
-        }
-        cout << endl;
-    }
-
-}
 
 //调度 
 void work() {
 
     ofstream out("/output/solution.txt");
+
+    //预处理-----------------------------------------------------------------------------------------------------/
+    vis = vector<bool>(cntServer, false);
+    sumBand = vector<ll>(cntTime, 0);
+    sortedTime = vector<int>(cntTime, 0);
+    maxServer = vector<int>(cntServer, 0);
+    for (int i = 0; i < cntTime; ++i) sortedTime[i] = i;
+
+    //计算每个时刻总流量
+    for (int i = 0; i < cntTime; ++i) {
+        for (int j = 0; j < cntClient; ++j) {
+            sumBand[i] += tclient[i][j];
+        }
+    }
+
+    //排序,流量大的时刻先排
+    sort(sortedTime.begin(), sortedTime.end(), [&](int a, int b) {
+        return sumBand[a] > sumBand[b];
+        });
 
     //哈希表：名字和索引对应
     for (int i = 0; i < cntClient; ++i) client_idx[client[i]] = i;
@@ -196,33 +196,40 @@ void work() {
         }
     }
 
-    //按边缘节点连接用户数量排序
-    for (int i = 0; i < cntClient; ++i) {
-        sort(workband[i].begin(), workband[i].end(), [&](int a, int b) {
-            return serverHas[a] < serverHas[b];
-        });
-    }
 
-
-    //按照用户能连接的边缘节点数量从小到大排序
-    vector<pair<int, int>> sortedClient;
+    //按照用户能连接的边缘节点数量，从小到大排序(边少的用户先选）
+    vector<pair<int, int>> sortedClient; //连接服务器数-序号
     for (int i = 0; i < cntClient; ++i) {
         int cnt = workband[i].size(), idx = i;
         sortedClient.push_back({ cnt, idx });
     }
     sort(sortedClient.begin(), sortedClient.end(), [&](const pair<int, int>& a, const pair<int, int>& b) {
-        return a.first < b.first || (a.first == b.first && a.second < b.second);
+        return a.first < b.first;
         });
 
-    //处理
-    /*int idxLine = 0, maxLine = cntTime * cntClient - 1;*/
-    for (int t = 0; t < cntTime; ++t) { //时间
-        vector<int> bd = tserver;
 
+
+
+    //预处理-----------------------------------------------------------------------------------------------------/
+    vector<vector<string>> stlog(cntTime); //每个时刻日志
+
+    //处理
+    for (int timeIdx = 0; timeIdx < cntTime; ++timeIdx) { //时间
+        vector<int> bd = tserver;
+        int t = sortedTime[timeIdx];
         //存储日志组
         vector<unordered_map<string, int>> curlog(cntClient); //用户i(server, get)从边缘获得多少流量
 
+
+
         for (int k = 0; k < cntClient; ++k) { //用户
+
+            //优先选择连接多的、用过的服务器
+            for (int i = 0; i < cntClient; ++i) {
+                sort(workband[i].begin(), workband[i].end(), [&](int a, int b) {
+                    return serverHas[a] > serverHas[b] || vis[a] > vis[b];
+                    });
+            }
 
             int i = sortedClient[k].second; //用户序号
 
@@ -230,36 +237,24 @@ void work() {
             int need = tclient[t][i]; //所需流量
 
             if (!need) {
-                out << s << endl;
+                //out << s << endl;
+                stlog[t].push_back(s);
                 continue;
             }
 
-            int bn = workband[i].size(); //连接服务器数量
-            //存储日志
-            int avg_need = need / bn;
-            for (int j = 0; j < bn; ++j) {
-                int idx = workband[i][j];
-                if (bd[idx] == 0) continue;
-                int k = min(avg_need, bd[idx]);
-                need -= k;
-                bd[idx] -= k;
-                //s += "<" + server[idx] + "," + to_string(k) + ">,";
-                curlog[i][server[idx]] += k;
-                if (need == 0) break;
-            }
 
-            for (int j = 0; need && j < bn; ++j) {
+            for (int j = 0; need && j < workband.size(); ++j) {
                 int idx = workband[i][j];
                 if (bd[idx] == 0) continue;
                 int k = min(need, bd[idx]);
+                vis[idx] = 1;
                 need -= k;
                 bd[idx] -= k;
-                //s += "<" + server[idx] + "," + to_string(k) + ">,";
                 curlog[i][server[idx]] += k;
-                if (need == 0) break;
             }
-                
-            
+
+
+
         }
 
         //拼接日志
@@ -271,8 +266,17 @@ void work() {
             }
 
             s.pop_back();
-            out << s << endl;
+            stlog[t].push_back(s);
+            //out << s << endl;
         }
+
+        for (int q = 0; q < cntServer; ++q) maxServer[q] = max(maxServer[q], tserver[q] - bd[q]);
+    }
+
+
+    //存储日志
+    for (int i = 0; i < cntTime; ++i) {
+        for (string str : stlog[i]) out << str << endl;
     }
 
     out.close();
@@ -291,7 +295,6 @@ int main()
 
     readFile();
     work();
-    //debug(); //审核数据
 
     return 0;
 }
